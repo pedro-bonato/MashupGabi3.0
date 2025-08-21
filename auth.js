b62678e300ed991ccc4ce1fb4d3151b10a98cb94
Gabriel Sganderla <gabriel.sganderla@cresol.com.br>
2025-08-21 10:16:09 -0300

    ajuste auth

--- auth.js
+++ auth.js
@@ -3,15 +3,3 @@ function iniciarAutorizacao() {
     resolve(); // já autoriza sem esperar clique
   });
 }
-
-window.addEventListener('load', function() {
-      const loadingScreen = document.getElementById('loading-screen');
-      
-      setTimeout(function() {
-        loadingScreen.classList.add('hidden');
-        // Remove a tela do DOM após a transição para não atrapalhar
-        setTimeout(() => {
-            loadingScreen.style.display = 'none';
-        }, 500); // Deve corresponder à duração da transição do CSS
-      }, 3000); // 3000 milissegundos = 3 segundos
-    });
--- carrossel.js
+++ carrossel.js
@@ -1,3 +1,15 @@
+window.addEventListener('load', function() {
+      const loadingScreen = document.getElementById('loading-screen');
+      
+      setTimeout(function() {
+        loadingScreen.classList.add('hidden');
+        // Remove a tela do DOM após a transição para não atrapalhar
+        setTimeout(() => {
+            loadingScreen.style.display = 'none';
+        }, 500); // Deve corresponder à duração da transição do CSS
+      }, 3000); // 3000 milissegundos = 3 segundos
+    });
+
 // Espera o documento carregar para garantir que todos os elementos existem
 document.addEventListener('DOMContentLoaded', function() {
     const slider = document.getElementById('slider');
